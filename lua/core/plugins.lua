268971051cdc796942c812060e29768384aa466e
csamga <gabin.mascioli@gmail.com>
2023-03-03 10:23:24 +0100

    added plugins: alpha-nvim, nvim-colorizer.lua, cleaned up config

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -13,18 +13,41 @@ end
 
 local packer_bootstrap = ensure_packer()
 
+-- Auto source and re-compile Packer when saving plugins.lua
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', {
+	command = 'source <afile> | silent! LspStop | silent! LspStart | PackerSync',
+	group = packer_group,
+	pattern = vim.fn.expand(vim.fn.stdpath('config')..'/lua/core/plugins.lua'),
+})
+
+-- Packer configuration
+require('packer').init {
+	display = {
+		open_fn = function()
+			local result, window, buffer = require('packer.util').float {
+				border = {
+					{ '╭', 'FloatBorder' },
+					{ '─', 'FloatBorder' },
+					{ '╮', 'FloatBorder' },
+					{ '│', 'FloatBorder' },
+					{ '╯', 'FloatBorder' },
+					{ '─', 'FloatBorder' },
+					{ '╰', 'FloatBorder' },
+					{ '│', 'FloatBorder' },
+				},
+			}
+			vim.api.nvim_win_set_option(window, 'winhighlight', 'NormalFloat:Normal')
+			return result, window, buffer
+		end,
+	},
+	max_jobs = 10,
+}
+
 -- Plugins
 require('packer').startup {
 	function(use)
 		use 'wbthomason/packer.nvim' -- Packer can manage itself
-
-		use { -- Startup screen
-			'goolord/alpha-nvim',
-			requires = 'nvim-tree/nvim-web-devicons',
-			config = function ()
-				require('alpha').setup(require('alpha.themes.dashboard').config)
-			end
-		}
 
   	use {	-- LSP
   		'neovim/nvim-lspconfig',
@@ -78,8 +101,8 @@ require('packer').startup {
 
 		use { -- File explorer
 			'nvim-tree/nvim-tree.lua',
-			requires = 'nvim-tree/nvim-web-devicons',
 			config = require('core.plugin-config.nvim-tree').config,
+			after = 'nvim-web-devicons',
 		}
 
   	use { -- Fuzzy finder
@@ -126,7 +149,7 @@ require('packer').startup {
   		config = require('core.plugin-config.autotag').config,
   	}
 
-		use {
+		use { -- Smart comments
 			'numToStr/Comment.nvim',
 			config = function ()
 				require('Comment').setup {}
@@ -146,44 +169,43 @@ require('packer').startup {
 
     use { -- Status line
     	'nvim-lualine/lualine.nvim',
-    	requires = {
-    		'kyazdani42/nvim-web-devicons',
-    		opt = true,
-    	},
     	after = 'vscode.nvim',
     	config = require('core.plugin-config.lualine').config,
     }
+
+		use { -- Icons
+			'nvim-tree/nvim-web-devicons',
+			config = function ()
+				require('nvim-web-devicons').setup {}
+			end,
+			after = 'vscode.nvim',
+		}
 
   	use { -- Indentation guides
   		'lukas-reineke/indent-blankline.nvim',
   		config = require('core.plugin-config.indent-blankline').config,
   	}
 
+		use { -- Startup screen
+			'goolord/alpha-nvim',
+			config = function ()
+				require('alpha').setup(require('alpha.themes.dashboard').config)
+			end,
+			after = 'nvim-web-devicons',
+		}
+
+		use { -- Highlight color constants
+			'norcalli/nvim-colorizer.lua',
+			config = function ()
+				require('colorizer').setup {}
+			end,
+			after = 'vscode.nvim',
+		}
+
 		if packer_bootstrap then
 			require('packer').sync()
 		end
 	end,
-
-	config = {
-		display = {
-			open_fn = function()
-				local result, window, buffer = require('packer.util').float {
-					border = {
-						{ '╭', 'FloatBorder' },
-						{ '─', 'FloatBorder' },
-						{ '╮', 'FloatBorder' },
-						{ '│', 'FloatBorder' },
-						{ '╯', 'FloatBorder' },
-						{ '─', 'FloatBorder' },
-						{ '╰', 'FloatBorder' },
-						{ '│', 'FloatBorder' },
-					},
-				}
-				vim.api.nvim_win_set_option(window, 'winhighlight', 'NormalFloat:Normal')
-				return result, window, buffer
-			end,
-		},
-	},
 }
 
 if packer_bootstrap then
@@ -194,11 +216,3 @@ if packer_bootstrap then
 	print '╰─────────────────────────────────╯'
 	return
 end
-
--- Auto source and re-compile Packer when saving init.lua
---local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
---vim.api.nvim_create_autocmd('BufWritePost', {
---	command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
---	group = packer_group,
---	pattern = vim.fn.expand '$MYVIMRC',
---})
