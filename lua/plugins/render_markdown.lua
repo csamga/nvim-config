3665483b1a625d3d0fa06d6648a115740e11ee70
csamga <gabin.mascioli@gmail.com>
2025-05-14 10:18:05 +0200

    added plugins

---
+++ lua/plugins/render_markdown.lua
@@ -0,0 +1,13 @@
+local spec = {
+   'MeanderingProgrammer/render-markdown.nvim',
+   ft = { 'markdown' },
+   dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons'
+   },
+   ---@module 'render-markdown'
+   ---@type render.md.UserConfig
+   opts = {},
+}
+
+return spec
---
+++ lua/plugins/zen_mode.lua
@@ -0,0 +1,12 @@
+local spec = {
+   'folke/zen-mode.nvim',
+   event = 'BufWinEnter',
+   opts = {
+      window = {
+         width = 87,
+         options = {}
+      }
+   },
+}
+
+return spec
